41549553a62344203e19160f5690273795f19c26
Fredlina <fdrxlynmrne@gmail.com>
2025-07-12 08:51:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.write("Hello, *World!* :sunglasses:")
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
